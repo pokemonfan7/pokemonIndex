4121428777636c05e73d211498c84513e038c08d
Alexz <rumblesoso7@gmail.com>
2018-08-07 10:29:37 +0800

    pokemon.json

--- src/shared/component/pokemon-item/pokemon-item.component.ts
+++ src/shared/component/pokemon-item/pokemon-item.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PokemonItemComponent implements OnInit {
     @Input() pokemon;
     src: string;
-    defaultSrc = '/assets/pokemon/body_bg.png';
+    defaultSrc = '/assets/pokemon/132.png';
 
     constructor() {
     }
