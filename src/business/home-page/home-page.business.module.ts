649cb81355ac079e19e5b27ec092eeaafe815549
Alexz <rumblesoso7@gmail.com>
2018-08-07 15:40:37 +0800

    delete antd

--- src/business/home-page/home-page.business.module.ts
+++ src/business/home-page/home-page.business.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HomePageComponent} from './home-page.component';
 import {RouterModule} from '@angular/router';
-import {NgZorroAntdModule} from 'ng-zorro-antd';
 import {SharedModule} from '../../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    NgZorroAntdModule,
     SharedModule,
   ],
   declarations: [
